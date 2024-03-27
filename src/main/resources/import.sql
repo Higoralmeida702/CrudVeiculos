04bcf035f688d5232ae1cdf1f77285cf67a546d6
Higoralmeida702 <153138268+Higoralmeida702@users.noreply.github.com>
2024-03-27 00:19:10 -0300

    adicao arquivo import.sql
    Este conjunto de comandos SQL realiza inserções de dados em duas tabelas, tb_department e tb_veiculos, para representar informações sobre departamentos e veículos.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_department(name) VALUES ('Carros');
+INSERT INTO tb_department(name) VALUES ('Motos');
+
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (1, 'Fiat', 'Uno', '2013');
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (1, 'Chevrolet', 'Camaro', '2009');
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (1, 'Toyota', 'Panda Trueno', '1983');
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (1, 'Renault', 'Sandero', '2015');
+
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (2, 'Honda', 'CG 125', '2013');
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (2, 'Yamaha', 'Fazer 250', '2009');
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (2, 'Suzuki', 'GSX-R750', '1983');
+INSERT INTO tb_veiculos(department_id, marca, modelo, ano) VALUES (2, 'Kawasaki', 'Ninja 300', '2015');
